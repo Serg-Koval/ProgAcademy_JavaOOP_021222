c4c4cb0214647a75425fa623565aa78dab5ec6a0
Serg-Koval <121090397+Serg-Koval@users.noreply.github.com>
2022-12-27 20:51:22 +0200

    Add files via upload

---
+++ HW Lesson 5_adv/FileComparator.java
@@ -0,0 +1,41 @@
+package sample;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+public class FileComparator {
+
+	public static void compareTwoFiles(File file1, File file2) throws IOException {
+
+		int bytes1 = 0;
+		int bytes2 = 0;
+		
+		byte[] bytesArray1 = new byte[1_000_000];
+		byte[] bytesArray2 = new byte[1_000_000];
+
+		try (InputStream reader1 = new FileInputStream(file1); InputStream reader2 = new FileInputStream(file2)) {
+			
+			for (;;) {
+				bytes1 = reader1.read(bytesArray1);
+				bytes2 = reader2.read(bytesArray2);
+
+				if (bytes1 <= 0 && bytes2 <= 0) {
+					break;
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		if(Arrays.equals(bytesArray1, bytesArray2)) {
+			System.out.println("The files are the same");
+		} else {
+			System.out.println("The files are different");
+		}
+
+	}
+}
---
+++ HW Lesson 5_adv/Main.java
@@ -0,0 +1,20 @@
+package sample;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		File file1 = new File("/Java_Start20221026_projects/OOP_Lesson5_HW_adv/test1.txt");
+		File file2 = new File("/Java_Start20221026_projects/OOP_Lesson5_HW_adv/test2.txt");
+		
+		try {
+			FileComparator.compareTwoFiles(file1, file2);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
